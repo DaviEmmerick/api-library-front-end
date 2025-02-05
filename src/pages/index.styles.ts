cf37a9aede3eefe4b10a288e941d6a5abe649e55
DaviEmmerick <163426242+DaviEmmerick@users.noreply.github.com>
2025-02-05 20:11:22 -0300

    style: home page styles

--- src/pages/index.styles.ts
+++ src/pages/index.styles.ts
@@ -11,6 +11,92 @@ export const Background = styled.div`
 
 export const Header = styled.div`
   width: 100%;
-  max-height: 5rem;
-  background-color: red;
+  height: 3.5rem;
+  background-color: rgba(0, 0, 0, 0.4);
+`;
+
+export const Options = styled.div`
+  display: flex;
+  justify-content: space-between;
+  font-size: 1rem;
+  color: white;
+  align-content: center;
+  max-width: 50rem;
+
+`;
+
+export const Span = styled.span`
+  font-size: .7rem;
+  color: white;
+`;
+
+export const Title = styled.p`
+  font-size: 1.6rem;
+  color: #FF9E40;
+  max-width: 3.5rem;
+  margin-left: 8rem;
+  line-height: 1.5rem;
+`;
+
+export const SubTitles = styled.div`
+  display: flex;
+  justify-content: space-between;
+  gap: 5rem;
+  align-items: center;
+`;
+
+export const Information = styled.div`
+  margin-left: 8rem;
+  margin-block: 10rem;
+  max-width: 30rem;
+  background-color: rgba(0, 0, 0, 0.4);
+  border-radius: .2rem;
+  
+`;
+
+export const Text = styled.p`
+  color: #FF9E40;
+  font-size: 2.62rem;
+`;
+
+export const Colors = styled.span`
+  color: white;
+`;
+
+export const Slogan = styled.p`
+  color: white;
+  font-size: 1rem;
+  margin-block: 1.5rem;
+`;
+
+export const About = styled.button`
+  width: 8rem;
+  height: 2.7rem;
+  font-weight: bold;
+  background-color: white; 
+  border: 2px solid transparent;
+  font-size: .9rem;
+  transition: background-color 0.8s ease, color 0.8s ease, border-color 0.8s ease;
+
+  &:hover {
+    background-color: black; 
+    color: white; 
+    border-color: black; 
+  }
+`;
+
+export const Contact = styled.button `
+  border: .1rem solid white;
+  width: 8rem;
+  height: 2.7rem;
+  color: white;
+  font-weight: bold;
+  border: 2px solid white;
+  margin-left: 1.5rem;
+  transition: background-color 0.8s ease, color 0.8s ease, border-color 0.8s ease;
+
+  &:hover {
+    background-color: white; 
+    color: black; 
+  }
 `;
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,32 @@
-import { Background, Header } from "./index.styles";
+import { Background, Header, Options, Title, Span, SubTitles, Text, Information, Slogan, Colors, About, Contact } from "./index.styles"; 
 
 export const HomePage = () => {
   return (
     <div>
       <Background>
-        <Header />
+        <Header>
+          <Options>
+            <div>
+              <Title><strong><Span>LIVRARIA</Span> LUMO</strong></Title>
+            </div>
+            <SubTitles>
+              <a href ="#criar">CRIAR LIVRO</a>
+              <a href ="#deletar">DELETAR LIVRO</a>
+              <a href ="#sugestoes">SUGESTÕES</a>
+            </SubTitles>
+          </Options>
+        </Header>
+
+
+        <Information>
+          <Text>Iluminando <Colors>histórias, inspirando leitores</Colors></Text>
+          <Slogan>Um lugar ideal para criar, avaliar e pedir sugestões de novos livros!</Slogan>
+
+          <About> SOBRE NÓS</About> 
+          <Contact> CONTATO </Contact>
+
+        </Information>
+
       </Background>
     </div>
   );
